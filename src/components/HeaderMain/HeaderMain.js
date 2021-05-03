7073b10b6df57c477effdbe108d2a710d5644666
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-05-03 13:21:49 +0600

    added social media icons

--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import './HeaderMain.css';
 import Typewriter from 'typewriter-effect';
+import linkedin from '../../images/linkedin.png';
+import github from '../../images/github.png';
+import behance from '../../images/behance.png';
+import facebook from '../../images/facebook.png';
+import twitter from '../../images/twitter.png';
 
 
 const HeaderMain = () => {
     return (
         <section className='main-header'>
             <div className="row">
-                <div className="col"></div>
+                <div className="col d-flex align-items-center justify-content-center">
+                    <article>
+                        <a href={`https://${'www.linkedin.com/in/sabbir185/'}`} target='_blank'><img src={linkedin} alt="" className="img-fluid social-icon d-block mb-3"/></a>
+
+                        <a href={`https://${'github.com/Sabbir185'}`} target='_blank'><img src={github} alt="" className="img-fluid social-icon d-block mb-3"/></a>
+
+                        <a href={`https://${'www.behance.net/sabbir185'}`} target='_blank'><img src={behance} alt="" className="img-fluid social-icon d-block mb-3"/></a>
+
+                        <a href={`https://${'www.facebook.com/sabbir185/'}`} target='_blank'><img src={facebook} alt="" className="img-fluid social-icon d-block mb-3"/></a>
+                        
+                        <a href={`https://${'twitter.com/sabbir185'}`} target='_blank'><img src={twitter} alt="" className="img-fluid social-icon d-block"/></a>
+                    </article>
+                </div>
                 <div className="col-8 col-lg-6 col-xlg-6">
                     <h4 className="text-success main-hello">Hi, my name is</h4>
                     <h1 className="main-title">SABBIR AHMMED</h1>
